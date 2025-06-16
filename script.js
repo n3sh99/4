c8009793bcd2afa76fa91fc939a5e905370b9ac9
n3sh99 <lucas.struz@escola.pr.gov.br>
2025-06-16 07:30:38 -0300

    m

--- script.js
+++ script.js
@@ -4,5 +4,6 @@ const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 botaoMostraPalavras.addEventListener("click", mostraPalavrasChave); 
 
 function mostraPalavrasChave() { 
-  alert("fui clicado!"); 
 } 
+const texto = document.querySelector("#entrada-de-texto").value;
+campoResultado.textContent = texto;
