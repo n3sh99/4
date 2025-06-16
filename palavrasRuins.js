947cc5364fbd55f028de1c1c554dbb1b9460a506
n3sh99 <lucas.struz@escola.pr.gov.br>
2025-06-16 07:44:12 -0300

    m

---
+++ palavrasRuins.js
@@ -0,0 +1,11 @@
+export const PALAVRAS_RUINS = new Set([
+  "que",
+
+  "para",
+
+  "com",
+
+  "não",
+
+  // restante da lista...
+]);
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import { PALAVRAS_RUINS } from "./palavrasRuins.js";
+
 const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 
 botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
