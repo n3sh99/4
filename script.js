4bcd20f1b55e22e39763d6ffbaeafc8c2ce2fc67
n3sh99 <lucas.struz@escola.pr.gov.br>
2025-06-23 07:44:14 -0300

    m

--- script.js
+++ script.js
@@ -2,10 +2,6 @@ import { PALAVRAS_RUINS } from "./palavrasRuins.js";
 
 const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
 
-bimport { PALAVRAS_RUINS } from "./palavrasRuins.js";
-
-const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
-
 botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 
 function mostraPalavrasChave() {
